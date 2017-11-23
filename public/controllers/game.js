548b5535488e041887c707bf5cf0507066d980c1
sarali91 <saraliwithme@gmail.com>
2017-11-22 22:45:41 -0600

    post active image to game page

--- public/controllers/game.js
+++ public/controllers/game.js
@@ -3,12 +3,21 @@ var router = express.Router();
 var ejs = require('ejs');
 //require for parsing json
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+router.use( bodyParser.json() );       // to support JSON-encoded bodies
+router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+router.use(express.json());       // to support JSON-encoded bodies
+router.use(express.urlencoded({
+  extended: true
+})); // to support URL-encoded bodies
 
 //handles post request for starting the game
-router.post('/', urlencodedParser, function(req, res) {
-	console.log(req.body);
-  res.render('game', {image:"heart"});
+router.post('/', function(req, res) {
+	console.log(req.body.image);
+  res.render('game', {image:req.body.image});
 });
 
 module.exports = router;
